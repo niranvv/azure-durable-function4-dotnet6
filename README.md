0f9071d43930d89ddb5a5dda59f69cc152899792
Niran Vijayakumar <niranvv@gmail.com>
2021-11-06 20:11:45 +0530

    Updated working code with DI & Startup file

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Azure Durable Functions v4 with .NET 6
-## Part 2 Completed
+## Part 3 Completed
---
+++ Startup.cs
@@ -0,0 +1,16 @@
+﻿using Learn.DurableFunction.ActivityFunctions;
+using Learn.DurableFunction.Services;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: FunctionsStartup(typeof(Learn.DurableFunction.Startup))]
+namespace Learn.DurableFunction
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            builder.Services.AddSingleton<IGitHubAPIService, GitHubAPIService>();
+        }
+    }
+}

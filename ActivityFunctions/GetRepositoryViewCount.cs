e59c4d0fdbb7ae2452347ddf83b660f0f0b00efa
Niran Vijayakumar <niranvv@gmail.com>
2021-11-07 18:01:20 +0530

    Throw Random Exceptions

--- ActivityFunctions/GetRepositoryViewCount.cs
+++ ActivityFunctions/GetRepositoryViewCount.cs
@@ -3,6 +3,7 @@ using Learn.DurableFunction.Services;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.DurableTask;
 using Microsoft.Extensions.Logging;
+using System;
 using System.Threading.Tasks;
 
 namespace Learn.DurableFunction.ActivityFunctions
@@ -19,8 +20,19 @@ namespace Learn.DurableFunction.ActivityFunctions
         [FunctionName(nameof(GetRepositoryViewCount))]
         public async Task<RepoViewCount> Run([ActivityTrigger] IDurableActivityContext context, ILogger log)
         {
+            ThrowRandomException();
             var repoName = context.GetInput<string>();
             return await _gitHubAPIService.GetRepositoryViewCount(repoName);
+        }
+
+        private static void ThrowRandomException()
+        {
+            var random = new Random();
+            var next = random.Next(11);
+            if (next >= 7)
+            {
+                throw new TooManyRequestsException("Random Excption thrown to test retry");
+            }
         }
     }
 }
---
+++ ActivityFunctions/TooManyRequestsException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Learn.DurableFunction.ActivityFunctions
+{
+    [Serializable]
+    internal class TooManyRequestsException : Exception
+    {
+        public TooManyRequestsException()
+        {
+        }
+
+        public TooManyRequestsException(string message) : base(message)
+        {
+        }
+
+        public TooManyRequestsException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected TooManyRequestsException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
